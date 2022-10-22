3fc5190ddf7f157cc261b300da6075da1a57f38f
samikia <kia.sami8@gmail.com>
2022-10-22 16:58:05 +0330

    power exercise

---
+++ power/power.js
@@ -0,0 +1,4 @@
+let firstNumber = Number(prompt("please insert first number", 0))
+let secondNumber = Number(prompt("please insert second number", 0))
+let result = (firstNumber ** secondNumber)
+alert("the result is" + result)
