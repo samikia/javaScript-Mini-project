b3f8b529d2f53120236a651e567923b2870b93b2
samikia <kia.sami8@gmail.com>
2022-10-22 16:07:40 +0330

    feat: avrage number

---
+++ avrage/avrage.js
@@ -0,0 +1,6 @@
+let firstNumber = Number(prompt("Enter first number:", 0))
+let secondNumber = Number(prompt("Enter second number:", 0))
+let thirdNumber = Number(prompt("Enter third number:", 0))
+
+let result = (firstNumber + secondNumber + thirdNumber) / 3
+alert(result)
